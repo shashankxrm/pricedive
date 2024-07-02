d875317059385e393fcbcd57f53eeac728b5a2bd
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-07-02 22:53:24 +0530

    added files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import Image from 'next/image'
-import SearchBar from '../components/SearchBar'
-import HeroCarousel from '@/components/HeroCarousel'
+import HeroCarousel from "@/components/HeroCarousel"
 
-const Home = () => {
+import Image from "next/image"
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
+import Searchbar from "@/components/SearchBar"
+import { Product } from "@/types/types"
+
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -21,24 +26,26 @@ const Home = () => {
 
             <h1 className="head-text">
               Unleash the Power of
-              <span className="text-primary"> PriceDive</span>
+              <span className="text-primary"> PriceWise</span>
             </h1>
 
             <p className="mt-6">
               Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
             </p>
 
-            <SearchBar />
+            <Searchbar />
           </div>
 
           <HeroCarousel />
         </div>
       </section>
-      <section className='trending-section'>
-        <h2 className='section-text'>Trending</h2>
-        <div className='flex flex-wrap gap-x-8 gap-y-16'>
-          {['Apple Iphone 13', 'Samsung Galaxy S21', 'OnePlus 9 Pro', 'Google Pixel 6'].map((product) => (
-            <div>{product}</div>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product: Product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,53 +1,48 @@
 "use client"
+
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
-import Image from 'next/image'
+import Image from "next/image";
 
 const heroImages = [
-    { imgUrl: "/assets/images/hero-1.svg", alt: 'smartwatch'},
-    { imgUrl: "/assets/images/hero-2.svg", alt: 'bag'},
-    { imgUrl: "/assets/images/hero-3.svg", alt: 'lamp'},
-    { imgUrl: "/assets/images/hero-4.svg", alt: 'air fryer'},
-    { imgUrl: "/assets/images/hero-5.svg", alt: 'chsir'}
+  { imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch'},
+  { imgUrl: '/assets/images/hero-2.svg', alt: 'bag'},
+  { imgUrl: '/assets/images/hero-3.svg', alt: 'lamp'},
+  { imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer'},
+  { imgUrl: '/assets/images/hero-5.svg', alt: 'chair'},
 ]
+
 const HeroCarousel = () => {
   return (
     <div className="hero-carousel">
-    <Carousel
-        showArrows={false}
+      <Carousel
         showThumbs={false}
-        showStatus={false}
+        // autoPlay
         infiniteLoop
-        autoPlay
-        interval={2000}
-        transitionTime={1000}
-    >
+        // interval={2000}
+        showArrows={false}
+        showStatus={false}
+      >
         {heroImages.map((image) => (
-            <Image 
-            key={image.alt}
+          <Image 
             src={image.imgUrl}
             alt={image.alt}
             width={484}
             height={484}
-            className='object-contain'
-            >
-                
-            </Image>
+            className="object-contain"
+            key={image.alt}
+          />
         ))}
-    </Carousel>
+      </Carousel>
 
-    <Image
-        src="/assets/icons/hand-drawn-arrow.svg"
+      <Image 
+        src="assets/icons/hand-drawn-arrow.svg"
         alt="arrow"
         width={175}
         height={175}
-        className='max-xl:hidden absolute -left-[-15%] bottom-0 z-0'
-    >
-
-    </Image>
-
+        className="max-xl:hidden absolute -left-[15%] bottom-0 z-0"
+      />
     </div>
-    
   )
 }
 
