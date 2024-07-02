873766cabb94d8b2b899cb66bb885c0883b75c9b
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-07-03 01:25:58 +0530

    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import HeroCarousel from "@/components/HeroCarousel"
-import Searchbar from "@/components/Searchbar"
+import Searchbar from "../components/Searchbar"
 import Image from "next/image"
 import { getAllProducts } from "@/lib/actions"
 import ProductCard from "@/components/ProductCard"
