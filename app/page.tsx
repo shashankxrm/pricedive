d7ac10a2799b59e8d20f892e32966b206fa44e57
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-07-03 15:04:13 +0530

    copied error files in to main files

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import HeroCarousel from "@/components/HeroCarousel"
 import Image from "next/image"
 import { getAllProducts } from "@/lib/actions"
 import ProductCard from "@/components/ProductCard"
-import Searchbar from "@/components/Searchbar"
+import { useState } from "react"
+import { FormEvent } from "react"
+import { scrapeAndStoreProduct } from "@/lib/actions"
+
 
 
 const Home = async () => {
@@ -50,6 +53,64 @@ const Home = async () => {
       </section>
     </>
   )
+}
+const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [ loading, setLoading ] = useState(false);
+  
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductLink(searchPrompt);
+
+    if(!isValidLink) {
+      alert('Please enter a valid Amazon product link');
+    }
+
+    try {
+      setLoading(true);
+      // Scraping logic here
+      const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <form
+      className="flex flex-wrap gap-4 mt-12"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === '' || loading}
+      > {loading? 'Searching...' : 'Search'}</button>
+    </form>
+  )
+}
+const isValidAmazonProductLink = (url: string) => {
+  try {
+    const parsedUrl = new URL(url);
+    const hostname = parsedUrl.hostname;
+
+    if(hostname.includes('amazon.com') || hostname.includes('amazon.') || hostname.includes('amazon')) {
+      return true;
+    }
+  }
+  catch (error) {
+    return false;
+  }
 }
 
 export default Home
